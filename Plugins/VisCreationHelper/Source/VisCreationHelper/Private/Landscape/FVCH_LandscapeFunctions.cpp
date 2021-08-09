5df0572cd730cbd86707bac1b44d8b4e69c0633c
Mihalo15z <66412311+Mihalo15z@users.noreply.github.com>
2021-08-09 18:07:03 +0300

    Add Landscape functions file

---
+++ Plugins/VisCreationHelper/Source/VisCreationHelper/Private/Landscape/FVCH_LandscapeFunctions.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Landscape/FVCH_LandscapeFunctions.h"
+
+FVCH_LandscapeFunctions::FVCH_LandscapeFunctions()
+{
+}
+
+FVCH_LandscapeFunctions::~FVCH_LandscapeFunctions()
+{
+}
---
+++ Plugins/VisCreationHelper/Source/VisCreationHelper/Public/Landscape/FVCH_LandscapeFunctions.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class VISCREATIONHELPER_API FVCH_LandscapeFunctions
+{
+public:
+	FVCH_LandscapeFunctions();
+	~FVCH_LandscapeFunctions();
+};
