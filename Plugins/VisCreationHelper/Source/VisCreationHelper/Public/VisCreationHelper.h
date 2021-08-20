10be1ac0eeda8ff5b4080b2190a8de367902775c
Mihalo15z <66412311+Mihalo15z@users.noreply.github.com>
2021-08-20 17:56:03 +0300

    transfer global structs

--- Plugins/VisCreationHelper/Source/VisCreationHelper/Public/VisCreationHelper.h
+++ Plugins/VisCreationHelper/Source/VisCreationHelper/Public/VisCreationHelper.h
@@ -27,3 +27,54 @@ private:
 private:
 	TSharedPtr<class FUICommandList> PluginCommands;
 };
+
+struct  VISCREATIONHELPER_API FDoubleVect2
+{
+	double X;
+	double Y;
+	//double& Lat() const { return X; }
+	//double& Lon() const { return Y; }
+
+	constexpr FDoubleVect2() :X(0.0), Y(0.0)
+	{};
+
+	constexpr FDoubleVect2(double x, double y) :X(x), Y(y)
+	{};
+
+	FORCEINLINE FVector operator=(const FDoubleVect2& rh)
+	{
+		return FVector(rh.X, rh.Y, 0.0f);
+	}
+	FString ToString()const
+	{
+		return FString::Printf(TEXT("X = %.9f, Y =%.9f"), X, Y);
+	}
+
+	bool EqualTo(const FDoubleVect2& OtherVal, double Epsilon = 0.000'000'001) const
+	{
+		return abs(OtherVal.X - X + OtherVal.Y - Y) < Epsilon;
+	}
+};
+
+struct FLevelImportData
+{
+public:
+
+	FString ToString() const;
+	FString ToCSVString() const;
+	static FLevelImportData FromString(FString StrData);
+	static FLevelImportData FromCSV(FString SrtData);
+	static FLevelImportData FromStringArray(const TArray<FString> & StrArrData);
+
+public:
+	
+	FDoubleVect2 LatAndLon;
+	FDoubleVect2 EndLatAndLon;
+	FDoubleVect2 CoordsXY;
+	FDoubleVect2 EndCoordsXY;
+	FDoubleVect2 SizeXY;
+	int32 ImportStatus = 0;
+	FString LevelName;
+
+};
+
