27cf2cdb7ced2eea339204b1e113cd566554bc32
Mihalo15z <66412311+Mihalo15z@users.noreply.github.com>
2021-08-20 17:55:20 +0300

    add xmlparser

--- Plugins/VisCreationHelper/Source/VisCreationHelper/VisCreationHelper.Build.cs
+++ Plugins/VisCreationHelper/Source/VisCreationHelper/VisCreationHelper.Build.cs
@@ -42,6 +42,7 @@ public class VisCreationHelper : ModuleRules
 				"Engine",
 				"Slate",
 				"SlateCore",
+                "XmlParser",
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
