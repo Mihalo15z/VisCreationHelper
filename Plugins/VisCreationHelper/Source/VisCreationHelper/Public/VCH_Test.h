d9969ee1589b402cbcd1a963ee06de6ff383fcc5
Mihalo15z <66412311+Mihalo15z@users.noreply.github.com>
2022-01-14 15:08:04 +0300

    Update VCH_Test.h

--- Plugins/VisCreationHelper/Source/VisCreationHelper/Public/VCH_Test.h
+++ Plugins/VisCreationHelper/Source/VisCreationHelper/Public/VCH_Test.h
@@ -46,13 +46,3 @@ public:
 
 };
 
-class VISCREATIONHELPER_API FVCHTimeConter
-{
-public:
-	FVCHTimeConter() = delete;
-	FVCHTimeConter(const FString& InCounterInfo);
-	~FVCHTimeConter();
-private:
-	FString CounterInfo;
-	int64 StartCicles;
-};
