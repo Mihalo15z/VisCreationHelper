cddf47e3ab70eb890df75e056035aaadec0b931c
Mihalo15z <66412311+Mihalo15z@users.noreply.github.com>
2022-04-18 12:20:30 +0300

    Add  material params names

--- Plugins/VisCreationHelper/Source/VisCreationHelper/Private/Settings/VCH_Settings.h
+++ Plugins/VisCreationHelper/Source/VisCreationHelper/Private/Settings/VCH_Settings.h
@@ -111,6 +111,10 @@ public:
 	UPROPERTY(Config, EditDefaultsOnly, Category = "Import|Paths")
 		FString GlobalImportContentDir = TEXT("_ImportData");
 
+	UPROPERTY(Config, EditDefaultsOnly, Category = "Import|Textures")
+		FString TParamName_BaseMask = TEXT("BaseTexture");
+	UPROPERTY(Config, EditDefaultsOnly, Category = "Import|Textures")
+		FString TParamName_WaterMas = TEXT("Water");
 	int32 GetFinalResolution()const
 	{
 		return Resolution * 2 + 1;
